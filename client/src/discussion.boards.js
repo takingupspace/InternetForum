7ce6bd6b9e906b0bd9c0318cc166428740c25477
takingupspace <belllabenthusiast@gmail.com>
2022-01-26 11:21:30 -0600

    discussion-thread-slugs-working

--- client/src/discussion.boards.js
+++ client/src/discussion.boards.js
@@ -1,15 +1,18 @@
 const discussionBoard = [
     {
-        id: 1,
+        id: 0,
         title: 'Scary Images',
         description: '[Filler Content]',
         image: 'NO IMAGE',
+        discussionBody: 'Scary movies are definitely my favorite genre, even though sometimes it disallows me from being ' +
+        "able to get a proper night's rest"
     },
     {
-        id: 2,
+        id: 1,
         title: 'Happy Images',
         description: '[Filler Content]',
-        image: 'NO IMAGE'
+        image: 'NO IMAGE',
+        discussionBody: 'This discussion thread is not only for memes but for funny cat pics!'
     }
 ]
 
--- client/src/pages/Discussion.js
+++ client/src/pages/Discussion.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import discussionBoard from "../discussion.boards";
 
-function Discussion(){
+function Discussion(props){
     let {id} = useParams();
     return (
+        <>
         <h1>This is the discussion page for thread # {id}</h1>
+        <p>{discussionBoard[id].discussionBody}</p>
+        </>
     )
 }
 
